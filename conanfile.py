b6579917f212780e053bbcd7a6e50126f9f2a369
Uilian Ries <uilianries@gmail.com>
2018-04-04 23:03:25 -0300

    Update recipe to Bincrafters template
    - Remove libcxx setting (pure_c)
    - Use concrete example from zstd
    - Add mingw build
    - Add clang 5 build
    - Add project logo
    - Add fPIC option
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -9,15 +9,17 @@ class ZstdConan(ConanFile):
     name = "zstd"
     version = "1.3.4"
     url = "https://github.com/bincrafters/conan-zstd"
+    homepage = "https://facebook.github.io/zstd/"
     description = "Zstandard - Fast real-time compression algorithm"
+    author = "Bincrafters <bincrafters@gmail.com>"
     license = "BSD"
     exports = ["LICENSE.md"]
     exports_sources = ['CMakeLists.txt']
     generators = 'cmake'
-    source_subfolder = "source_subfolder"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    options = {"shared": [True, False], "fPIC": [True, False]}
+    default_options = "shared=False", "fPIC=True"
+    source_subfolder = "source_subfolder"
 
     def source(self):
         source_url = "https://github.com/facebook/zstd"
@@ -25,20 +27,31 @@ class ZstdConan(ConanFile):
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
 
-    def build(self):
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+    def configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["ZSTD_BUILD_PROGRAMS"] = False
         cmake.definitions["ZSTD_BUILD_STATIC"] = not self.options.shared
         cmake.definitions["ZSTD_BUILD_SHARED"] = self.options.shared
-        cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.package_folder
-
+        if self.settings.os != "Windows":
+            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
         cmake.configure()
+        return cmake
+
+    def build(self):
+        cmake = self.configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
         self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
-        # all other files are copied by cmake.install()
+        cmake = self.configure_cmake()
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
