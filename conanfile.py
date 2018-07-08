21403125fe339eca1422603919332e457d04b78e
Uilian Ries <uilianries@gmail.com>
2018-07-08 10:05:19 -0300

    Bump version to 1.3.5
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -7,9 +7,9 @@ import os
 
 class ZstdConan(ConanFile):
     name = "zstd"
-    version = "1.3.4"
+    version = "1.3.5"
     url = "https://github.com/bincrafters/conan-zstd"
-    homepage = "https://facebook.github.io/zstd/"
+    homepage = "https://github.com/facebook/zstd"
     description = "Zstandard - Fast real-time compression algorithm"
     author = "Bincrafters <bincrafters@gmail.com>"
     license = "BSD"
@@ -22,8 +22,7 @@ class ZstdConan(ConanFile):
     source_subfolder = "source_subfolder"
 
     def source(self):
-        source_url = "https://github.com/facebook/zstd"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
         os.rename(extracted_dir, self.source_subfolder)
 
@@ -40,8 +39,6 @@ class ZstdConan(ConanFile):
         cmake.definitions["ZSTD_BUILD_PROGRAMS"] = False
         cmake.definitions["ZSTD_BUILD_STATIC"] = not self.options.shared
         cmake.definitions["ZSTD_BUILD_SHARED"] = self.options.shared
-        if self.settings.os != "Windows":
-            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
         cmake.configure()
         return cmake
 
@@ -50,9 +47,11 @@ class ZstdConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="license", src=self.source_subfolder)
+        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
         cmake = self.configure_cmake()
         cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
